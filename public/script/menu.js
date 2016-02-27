ab8baef4e62527e3af111839607643e7753777a2
dxnm <2711sq@gmail.com>
2016-02-27 10:57:27 +0700

    fix

--- public/script/menu.js
+++
@@ -1,9 +0,0 @@
-function is_touch_device() {
-return !!('ontouchstart' in window);
-}
-if(is_touch_device()) {
-	var ajax = new XMLHttpRequest();
-	ajax.open("GET", "/script/menu.html", false);
-	ajax.send();
-	document.body.innerHTML += ajax.responseText;
-}
