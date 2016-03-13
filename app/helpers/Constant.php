76d5544d2cdcaab86444e8a323bc03c500851d59
trantung <trantunghn196@gmail.com>
2016-03-13 14:54:08 +0700

    fix

--- app/helpers/Constant.php
+++ app/helpers/Constant.php
@@ -6,6 +6,10 @@ define('SEO', 3);
 //disable or enable
 define('ENABLED', 1);
 define('DISABLED', 2);
+define('ACTIVE', 1);
+define('INACTIVE', 2);
+define('ACTIVEUSER', 'Kích hoạt');
+define('INACTIVEUSER', 'Chưa kích hoạt');
 //pagination manager admin
 define('PAGINATE', 20);
 //pagination frontend
