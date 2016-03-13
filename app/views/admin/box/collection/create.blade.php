b05488d8f7aca7a08683b59c4dcd83dc288195b2
dxnm <2711sq@gmail.com>
2016-03-13 17:49:25 +0700

    fix

---
+++ app/views/admin/box/collection/create.blade.php
@@ -0,0 +1,80 @@
+@extends('admin.layout.default')
+
+@section('title')
+{{ $title='Thêm mới box tin' }}
+@stop
+
+@section('content')
+
+@include('admin.box.common')
+
+<div class="row">
+	<div class="col-xs-12">
+		<div class="box box-primary">
+		{{ Form::open(array('action' => array('BoxTypeController@store'), 'files' => true)) }}
+			<div class="box-body">
+				<div class="form-group">
+					<label for="name">Tên menu Vietnamese</label>
+					<div class="row">
+						<div class="col-sm-6">	                  	
+						   {{ Form::text('name_menu', null , textPlaceHolder('Tên thể loại tin')) }}
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="name">Tên content Vietnamese</label>
+					<div class="row">
+						<div class="col-sm-6">	                  	
+						   {{ Form::text('name_content', null , textPlaceHolder('Tên thể loại tin')) }}
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="name">Tên footer Vietnamese</label>
+					<div class="row">
+						<div class="col-sm-6">	                  	
+						   {{ Form::text('name_footer', null , textPlaceHolder('Tên thể loại tin')) }}
+						</div>
+					</div>
+				</div>
+				@foreach($arrayLang as $keyLang => $singLang)
+					<div class="form-group">
+						<label for="name">Tên menu {{ $singLang }}</label>
+						<div class="row">
+							<div class="col-sm-6">	                  	
+							   {{ Form::text($singLang.'_'.'name_menu', null , textPlaceHolder('Name')) }}
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="name">Tên content {{ $singLang }} </label>
+						<div class="row">
+							<div class="col-sm-6">	                  	
+							   {{ Form::text($singLang.'_'.'name_content', null , textPlaceHolder('Name')) }}
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="name">Tên footer {{ $singLang }} </label>
+						<div class="row">
+							<div class="col-sm-6">	                  	
+							   {{ Form::text($singLang.'_'.'name_footer', null , textPlaceHolder('Name')) }}
+							</div>
+						</div>
+					</div>
+				@endforeach
+				<div class="form-group">
+					<label>Upload ảnh</label>
+					{{ Form::file('image_url') }}
+				</div>
+
+				@include('admin.box.tab')
+		  
+				<div class="box-footer">
+					{{ Form::submit('Lưu lại', array('class' => 'btn btn-primary')) }}
+				</div>
+			</div>
+		{{ Form::close() }}
+	</div>
+</div>
+@stop
---
+++ app/views/admin/box/collection/index.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layout.default')
+
+@section('title')
+{{ $title='Quản lý box Collection' }}
+@stop
+
+@section('content')
+
+<!-- inclue Search form 
+
+-->
+<div class="row margin-bottom">
+	<div class="col-xs-12">
+		<a href="{{ action('BoxCollectionController@create') }}" class="btn btn-primary">Thêm box</a>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-xs-12">
+	  <div class="box">
+		<div class="box-header">
+		  <h3 class="box-title">Danh sách box</h3>
+		</div>
+		<div class="box-body table-responsive no-padding">
+			<table class="table table-hover">
+				<tr>
+					<th>ID</th>
+					<th>Tên box Vietnamese</th>
+					<th style="width:200px;">Action</th>
+				</tr>
+				@foreach(BoxCollection::whereIn('id', $list)->get() as $box)
+					<tr>
+						<td>{{ $box->id }}</td>
+						<td>{{ Common::getNameBox($box) }}</td>
+						<td>
+							<a href="{{ action('BoxCollectionController@edit', $box->id) }}" class="btn btn-primary">Sửa</a>
+							{{ Form::open(array('method'=>'DELETE', 'action' => array('BoxCollectionController@destroy', $box->id), 'style' => 'display: inline-block;')) }}
+							<button class="btn btn-danger" onclick="return confirm('Bạn có chắc chắn muốn xóa?');">Xóa</button>
+							{{ Form::close() }}
+						</td>
+					</tr>
+				@endforeach
+			</table>
+		</div>
+	  </div>
+	</div>
+</div>
+
+@stop
+
