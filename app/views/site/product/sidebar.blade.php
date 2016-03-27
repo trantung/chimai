d141d54efed4e18f204288c39a1922b7f7e6c250
trantung <trantunghn196@gmail.com>
2016-03-27 13:51:49 +0700

    fix

--- app/views/site/product/sidebar.blade.php
+++ app/views/site/product/sidebar.blade.php
@@ -1,8 +1,12 @@
+{{ Form::open() }}
 <div class="side">
 	<h3>{{ trans('captions.category') }}</h3>
 	<ul>
 		@foreach(Category::where('language', getLanguage())->get() as $cate )
-			<li><input type="checkbox" value="{{ $cate->id }}" /> {{ $cate->name }}</li>
+			<li>
+				{{ Form::checkbox('category[]', $cate->id ) }}
+				{{ $cate->name }}
+			</li>
 		@endforeach
 
 	</ul>
@@ -11,7 +15,7 @@
 	<h3>{{ trans('captions.surface') }}</h3>
 	<ul>
 		@foreach(Surface::where('language', getLanguage())->get() as $surface )
-			<li><input type="checkbox" value="{{ $surface->id }}" /> {{ $surface->name }}</li>
+			<li>{{ Form::checkbox('surface[]', $surface->id ) }} {{ $surface->name }}</li>
 		@endforeach
 	</ul>
 </div>
@@ -19,7 +23,7 @@
 	<h3>{{ trans('captions.material') }}</h3>
 	<ul>
 		@foreach(Material::where('language', getLanguage())->get() as $material )
-			<li><input type="checkbox" value="{{ $material->id }}" /> {{ $material->name }}</li>
+			<li>{{ Form::checkbox('material[]', $material->id ) }} {{ $material->name }}</li>
 		@endforeach
 	</ul>
 </div>
@@ -27,10 +31,11 @@
 	<h3>{{ trans('captions.size') }}</h3>
 	<ul>
 		@foreach(Size::where('language', getLanguage())->get() as $size )
-			<li><input type="checkbox" value="{{ $size->id }}" /> {{ $size->name }}</li>
+			<li>{{ Form::checkbox('size[]', $size->id ) }} {{ $size->name }}</li>
 		@endforeach
 	</ul>
 </div>
 <div class="side-submit">
 	<button class="button" title="Tìm kiếm" type="submit">{{ trans('captions.search') }}</button>
 </div>
+{{ Form::close() }}
