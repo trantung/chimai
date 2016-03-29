060a7f8385360a8f397a3428c4d27f1a5b21935e
trantung <trantunghn196@gmail.com>
2016-03-29 09:34:41 +0700

    fix

--- app/database/seeds/LanguageTableSeeder.php
+++ app/database/seeds/LanguageTableSeeder.php
@@ -4,38 +4,38 @@ class LanguageTableSeeder extends Seeder {
 
 	public function run()
 	{
-		AdminLanguage::create([
-			'model_name' => 'Surface',
-			'model_id' => 1,
-			'relate_name' => 'Surface',
-			'relate_id' => 2,
-			'status' => 1,
-			'weight_number' => 1,
-		]);
-		AdminLanguage::create([
-			'model_name' => 'Surface',
-			'model_id' => 3,
-			'relate_name' => 'Surface',
-			'relate_id' => 4,
-			'status' => 1,
-			'weight_number' => 2,
-		]);
-		AdminLanguage::create([
-			'model_name' => 'Size',
-			'model_id' => 1,
-			'relate_name' => 'Size',
-			'relate_id' => 2,
-			'status' => 1,
-			'weight_number' => 2,
-		]);
-		AdminLanguage::create([
-			'model_name' => 'Size',
-			'model_id' => 3,
-			'relate_name' => 'Size',
-			'relate_id' => 4,
-			'status' => 1,
-			'weight_number' => 2,
-		]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Surface',
+		// 	'model_id' => 1,
+		// 	'relate_name' => 'Surface',
+		// 	'relate_id' => 2,
+		// 	'status' => 1,
+		// 	'weight_number' => 1,
+		// ]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Surface',
+		// 	'model_id' => 3,
+		// 	'relate_name' => 'Surface',
+		// 	'relate_id' => 4,
+		// 	'status' => 1,
+		// 	'weight_number' => 2,
+		// ]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Size',
+		// 	'model_id' => 1,
+		// 	'relate_name' => 'Size',
+		// 	'relate_id' => 2,
+		// 	'status' => 1,
+		// 	'weight_number' => 2,
+		// ]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Size',
+		// 	'model_id' => 3,
+		// 	'relate_name' => 'Size',
+		// 	'relate_id' => 4,
+		// 	'status' => 1,
+		// 	'weight_number' => 2,
+		// ]);
 		AdminLanguage::create([
 			'model_name' => 'Origin',
 			'model_id' => 1,
@@ -52,38 +52,38 @@ class LanguageTableSeeder extends Seeder {
 			'status' => 1,
 			'weight_number' => 2,
 		]);
-		AdminLanguage::create([
-			'model_name' => 'Category',
-			'model_id' => 1,
-			'relate_name' => 'Category',
-			'relate_id' => 2,
-			'status' => 1,
-			'weight_number' => 2,
-		]);
-		AdminLanguage::create([
-			'model_name' => 'Category',
-			'model_id' => 3,
-			'relate_name' => 'Category',
-			'relate_id' => 4,
-			'status' => 1,
-			'weight_number' => 2,
-		]);
-		AdminLanguage::create([
-			'model_name' => 'Material',
-			'model_id' => 1,
-			'relate_name' => 'Material',
-			'relate_id' => 2,
-			'status' => 1,
-			'weight_number' => 2,
-		]);
-		AdminLanguage::create([
-			'model_name' => 'Material',
-			'model_id' => 3,
-			'relate_name' => 'Material',
-			'relate_id' => 4,
-			'status' => 1,
-			'weight_number' => 2,
-		]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Category',
+		// 	'model_id' => 1,
+		// 	'relate_name' => 'Category',
+		// 	'relate_id' => 2,
+		// 	'status' => 1,
+		// 	'weight_number' => 2,
+		// ]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Category',
+		// 	'model_id' => 3,
+		// 	'relate_name' => 'Category',
+		// 	'relate_id' => 4,
+		// 	'status' => 1,
+		// 	'weight_number' => 2,
+		// ]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Material',
+		// 	'model_id' => 1,
+		// 	'relate_name' => 'Material',
+		// 	'relate_id' => 2,
+		// 	'status' => 1,
+		// 	'weight_number' => 2,
+		// ]);
+		// AdminLanguage::create([
+		// 	'model_name' => 'Material',
+		// 	'model_id' => 3,
+		// 	'relate_name' => 'Material',
+		// 	'relate_id' => 4,
+		// 	'status' => 1,
+		// 	'weight_number' => 2,
+		// ]);
 
 	}
 
