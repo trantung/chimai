ecebe185d9528eeff6eb3ec8994e2258d2b7e059
trantung <trantunghn196@gmail.com>
2016-04-02 08:29:12 +0700

    ifx

--- app/lang/vi/label.php
+++ app/lang/vi/label.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'image_gallery' => 'Gallery', 
-	'product_relate' => 'Products relate', 
-	'product_request' => 'Send request', 
+	'image_gallery' => 'Hình ảnh phối cảnh', 
+	'product_relate' => 'Hình ảnh phối cảnh', 
+	'product_request' => 'Hình ảnh phối cảnh', 
 );
