d73ade15390dd6f1560903f1f4acc9684c798ff5
trantung <trantunghn196@gmail.com>
2016-04-28 09:29:42 +0700

    fix

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'abcgroupcompany@gmail.com', 'name' => 'MKD company'),
+	'from' => array('address' => 'companymkd@gmail.com', 'name' => 'MKD company'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'abcgroupcompany@gmail.com',
+	'username' => 'companymkd@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'Tunglaso1@abc123',
+	'password' => 'Tunglaso1@123',
 
 	/*
 	|--------------------------------------------------------------------------
