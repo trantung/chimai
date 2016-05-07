22591cbcb5bc0e760c1a28ebb4c6eeefacca354a
dxnm <2711sq@gmail.com>
2016-05-07 21:37:00 +0700

    fix

--- app/controllers/site/SiteIndexController.php
+++ app/controllers/site/SiteIndexController.php
@@ -224,10 +224,10 @@ class SiteIndexController extends SiteController {
 	public function search()
 	{
 		$input = Input::except('_token');
-		$products = Product::where('language', getLanguage())
-			->where('status', ACTIVE)
+		$products = Product::where('status', ACTIVE)
 			->where('name', 'LIKE', '%' . $input['keyword'] . '%')
 			->orWhere('code', 'LIKE', '%' . $input['keyword'] . '%')
+			->where('language', getLanguage())
 			->orderBy('created_at', 'desc')
 			->paginate(FRONENDPAGINATE);
 		$title = trans('messages.result_search');
